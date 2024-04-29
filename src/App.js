d11efebf2893d172b31378e5b77559c457e93e44
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-04-29 23:32:14 +0300

    image rotation instead of 3d model

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import TopBanner from './components/TopBanner';
-import ThreeDModel from './components/ThreeDModel';
+import RobotModel from './components/RobotModel';
 import TextImages from './components/TextImages';
 import ContactUs from './components/ContactUs';
 import Sponsors from './components/Sponsors';
 import OurTeam from './components/OurTeam';
 import Footer from './components/Footer';
-import './App.css';
-
 import GalleryOne from './components/galleryOne';
-// import GalleryTwo from './components/galleryTwo';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <TopBanner />
-      <ThreeDModel />
+      <RobotModel />
       <TextImages />
       <OurTeam />
       <GalleryOne />
       <Sponsors />
       <ContactUs />
       <Footer />
-      {/* <GalleryTwo /> */}
     </div>
   );
 }
---
+++ src/components/RobotModel.js
@@ -0,0 +1,68 @@
+import React, { useRef, useState, useEffect } from 'react';
+import '../styles/RobotModel.css'
+
+const RobotModel = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const imageRefs = useRef([]);
+
+  const baseUrl = 'https://www.apple.com/105/media/us/apple-vision-pro/2024/6e1432b2-fe09-4113-a1af-f20987bcfeee/anim/360/large/';
+  const imageCount = 199;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const totalScrollHeight = document.body.scrollHeight - window.innerHeight;
+      const scrollProgress = scrollPosition / totalScrollHeight;
+      const multiplier = 1.5; // Adjust this value to change scroll speed
+      const newImageIndex = Math.floor(scrollProgress * imageCount * multiplier);
+
+      // Ensure index stays within imageUrls bounds (added safeguard)
+      if (newImageIndex >= 0 && newImageIndex < imageCount) {
+        setCurrentImageIndex(newImageIndex);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="RobotModel" style={{ width: '100%', height: '70vh', marginBottom: '5%' }}>
+      <h1>Our 2024 Robot</h1>
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center' 
+        }}
+      >
+        {Array(imageCount) 
+          .fill(null)
+          .map((_, index) => (
+            <img
+              key={index}
+              ref={(el) => (imageRefs.current[index] = el)}
+              src={`${baseUrl}0${index.toString().padStart(3, '0')}.jpg`} 
+              alt={`Robot Image ${index + 1}`}
+              style={{
+                position: 'absolute',
+                top: '0%',
+                height: '100%',
+                opacity: index === currentImageIndex ? 1 : 0,
+                transition: 'none',
+              }}
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default RobotModel;
