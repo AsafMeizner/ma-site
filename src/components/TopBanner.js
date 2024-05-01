3cb3ab40779aff84ee9462c6905d268c40c92d27
Asaf Meizner <asafmeizner@gmail.com>
2024-05-01 10:53:54 +0300

    updated banner

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import '../styles/TopBanner.css'
+import '../styles/TopBanner.css';
 
 const TopBanner = () => {
   return (
     <div className="top-banner">
-      <img src="/banner.png" alt="Banner" className="banner-image" />
+      <img
+        src="/bannerBackground.JPG"
+        alt="Banner"
+        className="banner-background-image"
+      />
+      <div className="img-overlay">
+        <h1 className="banner-title">Makers Assemble #5951</h1>
+        <p className="banner-subtitle">Makers Lead The Way</p>
+        <img src="/ma-logo.png" alt="Team 5951 Logo" className="team-logo" />
+      </div>
     </div>
   );
 };
