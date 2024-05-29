e4c4c955a9368f686e0296991e077833329f1250
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-05-29 18:25:22 +0300

    Update RobotModel.js

--- src/components/RobotModel.js
+++ src/components/RobotModel.js
@@ -54,7 +54,6 @@ const RobotModel = () => {
               style={{
                 position: 'absolute',
                 top: '0%',
-                height: '100%',
                 width: '100%',
                 opacity: index === currentImageIndex ? 1 : 0,
                 transition: 'none',
