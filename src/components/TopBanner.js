a169690ee49e7d52eb3ff50af772919f9bd3c3ce
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-05-29 19:25:41 +0300

    logo based on portrait

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -2,6 +2,9 @@ import React from 'react';
 import '../styles/TopBanner.css';
 
 const TopBanner = () => {
+
+  const isPortrait = window.screen.width < window.screen.height;
+
   return (
     <div className="top-banner">
       <img
@@ -12,7 +15,14 @@ const TopBanner = () => {
       <div className="img-overlay">
         <h1 className="banner-title">Makers Assemble #5951</h1>
         <p className="banner-subtitle">Makers Lead The Way</p>
-        <img src="/ma-logo.png" alt="Team 5951 Logo" className="team-logo" />
+        <img 
+          src="/ma-logo.png" 
+          alt="Team 5951 Logo" 
+          style={{
+            height: isPortrait? '30%' : 'auto',
+            width: isPortrait? 'auto' : '30%',
+          }}
+        />
       </div>
     </div>
   );
