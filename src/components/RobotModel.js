46850709f5623680c92a9714a3f8a2757fe64cbd
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-05-29 18:27:50 +0300

    Update RobotModel.js

--- src/components/RobotModel.js
+++ src/components/RobotModel.js
@@ -31,7 +31,7 @@ const RobotModel = () => {
   }, []);
 
   return (
-    <div className="RobotModel" style={{height: '90vh'}}>
+    <div className="RobotModel">
       <div
         style={{
           position: 'relative',
