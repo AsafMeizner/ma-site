26091ace5c7e113310f4b5f71621d00d05e4747a
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-05-29 19:17:19 +0300

    made the logo change if portrait

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -2,12 +2,17 @@ import React from 'react';
 import '../styles/TopBanner.css';
 
 const TopBanner = () => {
+  const isPortrait = window.screen.width < window.screen.height;
+
   return (
     <div className="top-banner">
       <img
         src="/bannerBackground.JPG"
         alt="Banner"
-        className="banner-background-image"
+        style={{
+          heigt: 'auto',
+          width: isPortrait? '90%' : '30%'
+        }}
       />
       <div className="img-overlay">
         <h1 className="banner-title">Makers Assemble #5951</h1>
