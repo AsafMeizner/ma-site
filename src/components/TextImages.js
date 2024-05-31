3f0827700cd0836bcf73436ce58b73c39185fcc7
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-05-31 04:22:21 +0300

    sef

--- src/components/TextImages.js
+++ src/components/TextImages.js
@@ -19,18 +19,20 @@ const TextImages = () => {
 
   return (
     <div className={`text-images ${stacked ? 'stacked' : ''}`}>
-      <h2 style={{marginTop: '0px'}}>Section Title</h2>
-      <div className="row">
-        <img src="/image1.png" alt="Makers Assemble Logo" className="image" />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur eu eros et volutpat. Aliquam id condimentum ipsum, vitae sagittis ligula. Morbi sit amet dui vel mi placerat cursus. Quisque tempor mi ut tortor pulvinar vehicula. Quisque consectetur ex in tellus scelerisque consectetur. Phasellus placerat pharetra tortor, eu condimentum diam tincidunt non. In lobortis mollis risus quis lobortis. Mauris elementum neque sed purus cursus egestas. Nulla vitae molestie est. Sed pellentesque nunc sed dictum tristique. Pellentesque sed neque justo. In suscipit sem vel lacus placerat aliquet sed at lectus. Nulla a ornare metus, sit amet bibendum mi. Aenean vitae nunc nec justo scelerisque imperdiet et eu ligula. Praesent eu urna a diam eleifend molestie.</p>
-      </div>
-      <div className="row">
-        <img src="/image2.png" alt="2023 robot - Magnoom" className="image" />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur eu eros et volutpat. Aliquam id condimentum ipsum, vitae sagittis ligula. Morbi sit amet dui vel mi placerat cursus. Quisque tempor mi ut tortor pulvinar vehicula. Quisque consectetur ex in tellus scelerisque consectetur. Phasellus placerat pharetra tortor, eu condimentum diam tincidunt non. In lobortis mollis risus quis lobortis. Mauris elementum neque sed purus cursus egestas. Nulla vitae molestie est. Sed pellentesque nunc sed dictum tristique. Pellentesque sed neque justo. In suscipit sem vel lacus placerat aliquet sed at lectus. Nulla a ornare metus, sit amet bibendum mi. Aenean vitae nunc nec justo scelerisque imperdiet et eu ligula. Praesent eu urna a diam eleifend molestie.</p>
-      </div>
-      <div className="row">
-        <img src="/image3.png" alt="FRC" className="image" />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur eu eros et volutpat. Aliquam id condimentum ipsum, vitae sagittis ligula. Morbi sit amet dui vel mi placerat cursus. Quisque tempor mi ut tortor pulvinar vehicula. Quisque consectetur ex in tellus scelerisque consectetur. Phasellus placerat pharetra tortor, eu condimentum diam tincidunt non. In lobortis mollis risus quis lobortis. Mauris elementum neque sed purus cursus egestas. Nulla vitae molestie est. Sed pellentesque nunc sed dictum tristique. Pellentesque sed neque justo. In suscipit sem vel lacus placerat aliquet sed at lectus. Nulla a ornare metus, sit amet bibendum mi. Aenean vitae nunc nec justo scelerisque imperdiet et eu ligula. Praesent eu urna a diam eleifend molestie.</p>
+      <div style={{padding: '20px'}}>
+        <h2 style={{marginTop: '0px'}}>Section Title</h2>
+        <div className="row">
+          <img src="/image1.png" alt="Makers Assemble Logo" className="image" />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur eu eros et volutpat. Aliquam id condimentum ipsum, vitae sagittis ligula. Morbi sit amet dui vel mi placerat cursus. Quisque tempor mi ut tortor pulvinar vehicula. Quisque consectetur ex in tellus scelerisque consectetur. Phasellus placerat pharetra tortor, eu condimentum diam tincidunt non. In lobortis mollis risus quis lobortis. Mauris elementum neque sed purus cursus egestas. Nulla vitae molestie est. Sed pellentesque nunc sed dictum tristique. Pellentesque sed neque justo. In suscipit sem vel lacus placerat aliquet sed at lectus. Nulla a ornare metus, sit amet bibendum mi. Aenean vitae nunc nec justo scelerisque imperdiet et eu ligula. Praesent eu urna a diam eleifend molestie.</p>
+        </div>
+        <div className="row">
+          <img src="/image2.png" alt="2023 robot - Magnoom" className="image" />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur eu eros et volutpat. Aliquam id condimentum ipsum, vitae sagittis ligula. Morbi sit amet dui vel mi placerat cursus. Quisque tempor mi ut tortor pulvinar vehicula. Quisque consectetur ex in tellus scelerisque consectetur. Phasellus placerat pharetra tortor, eu condimentum diam tincidunt non. In lobortis mollis risus quis lobortis. Mauris elementum neque sed purus cursus egestas. Nulla vitae molestie est. Sed pellentesque nunc sed dictum tristique. Pellentesque sed neque justo. In suscipit sem vel lacus placerat aliquet sed at lectus. Nulla a ornare metus, sit amet bibendum mi. Aenean vitae nunc nec justo scelerisque imperdiet et eu ligula. Praesent eu urna a diam eleifend molestie.</p>
+        </div>
+        <div className="row">
+          <img src="/image3.png" alt="FRC" className="image" />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur eu eros et volutpat. Aliquam id condimentum ipsum, vitae sagittis ligula. Morbi sit amet dui vel mi placerat cursus. Quisque tempor mi ut tortor pulvinar vehicula. Quisque consectetur ex in tellus scelerisque consectetur. Phasellus placerat pharetra tortor, eu condimentum diam tincidunt non. In lobortis mollis risus quis lobortis. Mauris elementum neque sed purus cursus egestas. Nulla vitae molestie est. Sed pellentesque nunc sed dictum tristique. Pellentesque sed neque justo. In suscipit sem vel lacus placerat aliquet sed at lectus. Nulla a ornare metus, sit amet bibendum mi. Aenean vitae nunc nec justo scelerisque imperdiet et eu ligula. Praesent eu urna a diam eleifend molestie.</p>
+        </div>
       </div>
     </div>
   );
